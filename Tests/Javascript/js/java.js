a194e537bee54f34a1d70c36a340501acab8f10b
AldanaNemec <aldana.nemec@gmail.com>
2022-09-20 17:03:14 -0300

    Clase 20/9

---
+++ Tests/Javascript/js/java.js
@@ -0,0 +1,7 @@
+console.log("Starting with Javascript")
+console.log(36+1)
+document.write("<h3 style=\"color: red\">Incorporated text by JS</h3>")
+console.info(5)
+console.warn("Warning text")
+console.error("Error text")
+document.write("Hi!")
---
+++ Tests/Javascript/js/variables.js
@@ -0,0 +1,22 @@
+var edad
+console.log(edad) //undefined
+
+var edad2 = 25
+console.log(edad2)
+
+edad2 = 36 + 1
+console.log(edad2)
+
+edad2 = "Hi"
+console.log(typeof edad2)
+
+edad2 = true
+console.log(typeof edad2)
+
+edad2= (36 + 4) * 2
+console.log(typeof edad2)
+
+// const IVA = 21
+// console.log(IVA)
+// IVA = 10.5
+// console.log(IVA)
